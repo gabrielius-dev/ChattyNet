df8226d0ed4e00d9929b7c60f32e661792219259
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-04-25 21:39:24 +0300

    Add BrowserRouter and redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { CssBaseline } from "@mui/material";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
+import { BrowserRouter } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -9,7 +12,11 @@ import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <BrowserRouter>
+      <CssBaseline />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
