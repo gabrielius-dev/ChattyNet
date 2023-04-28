026a5d0f6773823fb24e059f87b2bc0a07633f4a
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-04-28 17:46:36 +0300

    Add redux and material theme providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
 import { BrowserRouter } from "react-router-dom";
+import { LIGHT_BLUE_COLOR } from "./styles/colors.ts";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./styles/styles.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: LIGHT_BLUE_COLOR,
+      contrastText: "#fff",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <CssBaseline />
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
