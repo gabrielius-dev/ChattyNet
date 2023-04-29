95d8afa2d180f7326b9fddd0acf356651cd07946
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-04-29 21:57:23 +0300

    Add LogInForm and SignUpForm

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,18 @@ import Profile from "./components/Profile/Profile";
 import { useAppSelector } from "./app/hooks";
 import LoginReminder from "./components/LoginReminder";
 import useLocationListener from "./app/functions/historyListener";
+import LogInForm from "./components/AuthForms/LogInForm";
+import SignUpForm from "./components/AuthForms/SignUpForm";
 
 function App() {
   const isLoggedIn = useAppSelector((state) => state.login.isLoggedIn);
+  const isLogInFormShowing = useAppSelector(
+    (state) => state.UI.isLogInFormShowing
+  );
+  const isSignUpFormShowing = useAppSelector(
+    (state) => state.UI.isSignUpFormShowing
+  );
+
   useLocationListener();
 
   return (
@@ -24,7 +33,9 @@ function App() {
         <Route path="/bookmarks" element={<Bookmarks />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-      {isLoggedIn && <LoginReminder />}
+      {isLogInFormShowing && <LogInForm />}
+      {isSignUpFormShowing && <SignUpForm />}
+      {!isLoggedIn && <LoginReminder />}
     </Box>
   );
 }
