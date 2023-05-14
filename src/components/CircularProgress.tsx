1e1b83282d091e07dea5608e832cf3f1c47b5324
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-05-14 18:06:58 +0300

    Refactor positioning

--- src/components/CircularProgress.tsx
+++ src/components/CircularProgress.tsx
@@ -2,15 +2,17 @@ import CircularProgress from "@mui/material/CircularProgress/CircularProgress";
 
 const CircularProgressComponent = () => {
   return (
-    <CircularProgress
-      size={100}
-      sx={{
-        position: "absolute",
+    <div
+      style={{
+        position: "fixed",
         top: "50%",
         left: "50%",
-        transform: "translate(-50%,-50%)",
+        transform: "translate(-50%, -50%)",
+        zIndex: 9999, // Adjust this value if needed
       }}
-    />
+    >
+      <CircularProgress size={100} />
+    </div>
   );
 };
 export default CircularProgressComponent;
