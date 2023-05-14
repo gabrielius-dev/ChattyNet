137b8277cc7e0d7285e9be1eae077f6f85fbed15
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-05-14 17:37:40 +0300

    Add additional UI states and functions

--- src/app/features/UISlice.ts
+++ src/app/features/UISlice.ts
@@ -1,13 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface UIState {
   isLogInFormShowing: boolean;
   isSignUpFormShowing: boolean;
+  isInitialSignUpFormShowing: boolean;
+  isCreateAccountFormShowing: boolean;
+  errorMessage: string;
+  isSnackbarOpen: boolean;
+  isLoginReminderShowing: boolean;
 }
 
 const initialState: UIState = {
   isLogInFormShowing: false,
   isSignUpFormShowing: false,
+  isInitialSignUpFormShowing: true,
+  isCreateAccountFormShowing: false,
+  errorMessage: "",
+  isSnackbarOpen: false,
+  isLoginReminderShowing: false,
 };
 
 export const UISlice = createSlice({
@@ -22,13 +32,54 @@ export const UISlice = createSlice({
     },
     showSignUpForm: (state) => {
       state.isSignUpFormShowing = true;
+      state.isInitialSignUpFormShowing = true;
     },
     hideSignUpForm: (state) => {
       state.isSignUpFormShowing = false;
+      state.isInitialSignUpFormShowing = false;
+      state.isCreateAccountFormShowing = false;
+    },
+    showInitialSignUpForm: (state) => {
+      state.isInitialSignUpFormShowing = true;
+    },
+    hideInitialSignUpForm: (state) => {
+      state.isInitialSignUpFormShowing = false;
+    },
+
+    showCreateAccountForm: (state) => {
+      state.isCreateAccountFormShowing = true;
+    },
+    hideCreateAccountForm: (state) => {
+      state.isCreateAccountFormShowing = false;
+    },
+    showLoginReminder: (state) => {
+      state.isLoginReminderShowing = true;
+    },
+    hideLoginReminder: (state) => {
+      state.isLoginReminderShowing = false;
+    },
+
+    setErrorMessage: (state, action: PayloadAction<string>) => {
+      state.errorMessage = action.payload;
+    },
+    setIsSnackbarOpen: (state, action: PayloadAction<boolean>) => {
+      state.isSnackbarOpen = action.payload;
     },
   },
 });
 
-export const { showLogInForm, hideLogInForm, showSignUpForm, hideSignUpForm } =
-  UISlice.actions;
+export const {
+  showLogInForm,
+  hideLogInForm,
+  showSignUpForm,
+  hideSignUpForm,
+  showInitialSignUpForm,
+  hideInitialSignUpForm,
+  showCreateAccountForm,
+  hideCreateAccountForm,
+  showLoginReminder,
+  hideLoginReminder,
+  setErrorMessage,
+  setIsSnackbarOpen,
+} = UISlice.actions;
 export default UISlice.reducer;
