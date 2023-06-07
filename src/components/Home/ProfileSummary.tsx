4e37311f3a751b539083f9913e995bc2549dd067
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-06-07 21:55:42 +0300

    Add ProfileSummary with functionality

---
+++ src/components/Home/ProfileSummary.tsx
@@ -0,0 +1,91 @@
+import { Avatar, Grid, Paper, Typography } from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+interface ProfileSummaryArguments {
+  photoURL: string | null;
+  fullName: string;
+  username: string;
+  information: string;
+  following: number;
+  followers: number;
+}
+
+export default function ProfileSummary({
+  photoURL,
+  fullName,
+  username,
+  information,
+  following,
+  followers,
+}: ProfileSummaryArguments) {
+  const [hovered, setHovered] = useState(false);
+  return (
+    <Paper elevation={1} sx={{ p: 1, borderRadius: "15px" }}>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Link
+            to={username}
+            style={{ color: "inherit", textDecoration: "none" }}
+          >
+            <Avatar src={photoURL ?? undefined} sx={{ cursor: "pointer" }}>
+              {!photoURL && username[0].toUpperCase()}
+            </Avatar>
+          </Link>
+        </Grid>
+        <Grid item container>
+          <Grid item xs={12}>
+            <Link
+              style={{
+                fontWeight: "bold",
+                wordBreak: "break-all",
+                color: "black",
+                textDecoration: hovered ? "underline" : "none",
+                cursor: "pointer",
+              }}
+              to={username}
+              onMouseOver={() => setHovered(true)}
+              onMouseOut={() => setHovered(false)}
+            >
+              {fullName}
+            </Link>
+          </Grid>
+          <Grid item xs={12}>
+            <Link
+              style={{
+                wordBreak: "break-all",
+                textDecoration: "none",
+                cursor: "pointer",
+                color: "#adadad",
+              }}
+              to={username}
+            >
+              @{username}
+            </Link>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>{information}</Typography>
+        </Grid>
+        <Grid item container xs={12}>
+          <Grid item container spacing={1} xs={6}>
+            <Grid item>
+              <Typography fontWeight="bold">{following}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography color="#adadad">Following</Typography>
+            </Grid>
+          </Grid>
+          <Grid item container spacing={1} xs={6}>
+            <Grid item>
+              <Typography fontWeight="bold">{followers}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography color="#adadad">Followers</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
