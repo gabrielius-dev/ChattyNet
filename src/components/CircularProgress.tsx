a12d51a0ad996252473436468b45f258dc2bc341
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-07-02 15:09:46 +0300

    Remove comment

--- src/components/CircularProgress.tsx
+++ src/components/CircularProgress.tsx
@@ -8,7 +8,7 @@ const CircularProgressComponent = () => {
         top: "50%",
         left: "50%",
         transform: "translate(-50%, -50%)",
-        zIndex: 9999, // Adjust this value if needed
+        zIndex: 9999,
       }}
     >
       <CircularProgress size={100} />
